e97d399ae4dbfe43cc064a5e8dc241974f80b8c0
AzureFennec <carnauris@gmail.com>
2026-01-30 15:46:19 -0800

    Expanded pipeline for processing requests

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 from flask import Flask, request, redirect, send_file, abort, jsonify
-from flask_cors import CORS
+
 import os
 
 app = Flask(__name__)
-CORS(app)
 
 STAFF_PASSWORD = 'EERIE'
 
 @app.route("/")
 def home():
-    return send_file("index.html")
+    return send_file("test_index.html")
 
 @app.route("/staff_login")
 def staff_sign():
-    return send_file("staff_login.html")
+    return send_file("test_staff_login.html")
 
 @app.route("/orders",methods=['GET'])
 def pull_page():
@@ -21,16 +20,77 @@ def pull_page():
 
     if token != STAFF_PASSWORD:
         abort(403)
-    return send_file("assign_pulls.html")
+    return send_file("test_assign_pulls.html")
 
-@app.route("/assign_pulls", methods=["GET"])
+@app.route("/load_employees", methods=["GET"])
 def fetch_orders():
     from Pull_Automator.employees import GathingSkills
+    employees = GathingSkills()  # this is your dictionary
+
+    employees_dict = employees.employees
+    return jsonify({
+        "employees": employees_dict
+    })
+
+@app.route("/assign_pulls", methods=["POST"])
+def assign_pulls():
+    data = request.get_json()
+
     from Pull_Automator.orders import OrderSheet
     from Pull_Automator.automator import assign_pulls
     orders = OrderSheet().orders
-    employees = GathingSkills()
-    ass = assign_pulls(employees, orders)
+    ass = assign_pulls(data, orders)
+    print(ass.unassigned_items)
+    return jsonify({
+        "columns": ass.assignments.columns.tolist(),
+        "assignments": ass.assignments.to_dict(orient="records"),
+        "unassigned_items": ass.unassigned_items,
+        "unrecognized_items": ass.unrecognized_items
+    })
+
+from flask import Flask, request, redirect, send_file, abort, jsonify
+
+import os
+
+app = Flask(__name__)
+
+STAFF_PASSWORD = 'EERIE'
+
+@app.route("/")
+def home():
+    return send_file("test_index.html")
+
+@app.route("/staff_login")
+def staff_sign():
+    return send_file("test_staff_login.html")
+
+@app.route("/orders",methods=['GET'])
+def pull_page():
+    token = request.args.get("key")
+
+    if token != STAFF_PASSWORD:
+        abort(403)
+    return send_file("test_assign_pulls.html")
+
+@app.route("/load_employees", methods=["GET"])
+def fetch_orders():
+    from Pull_Automator.employees import GathingSkills
+    employees = GathingSkills()  # this is your dictionary
+
+    employees_dict = employees.employees
+    return jsonify({
+        "employees": employees_dict
+    })
+
+@app.route("/assign_pulls", methods=["POST"])
+def assign_pulls():
+    data = request.get_json()
+
+    from Pull_Automator.orders import OrderSheet
+    from Pull_Automator.automator import assign_pulls
+    orders = OrderSheet().orders
+    ass = assign_pulls(data, orders)
+    print(ass.unassigned_items)
     return jsonify({
         "columns": ass.assignments.columns.tolist(),
         "assignments": ass.assignments.to_dict(orient="records"),
